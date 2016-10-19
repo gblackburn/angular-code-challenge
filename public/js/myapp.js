2ad08dc40db59bdfd1e3ef8fdf31d7abbeb7797f
Grace Blackburn <graceblackburn@Graces-MacBook-Pro.local>
2016-10-19 00:08:58 -0700

    "rounded score" filter

--- public/js/myapp.js
+++ public/js/myapp.js
@@ -1,4 +1,10 @@
 var app = angular.module('myApp', []);
+app.filter('roundedScore', function() {
+  return function(score) {
+    var rounded = Math.round(score / 10) * 10;
+    return rounded;
+  }
+});
 app.controller('dealsCtrl', function($scope, $http) {
   $scope.dealsData = "Loading...";
   $http.get("http://www.cheapshark.com/api/1.0/deals").then(function (response) {
