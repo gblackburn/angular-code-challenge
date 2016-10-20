554145195773f4ad9aca5899bc487251566fcba7
Grace Blackburn <graceblackburn@Graces-MacBook-Pro.local>
2016-10-19 18:22:45 -0700

    adding local json file, cleaning up UI, removing unused variables and console.logs, changing Math.round to Math.ceil, adding filter to order object in reverse

--- public/js/myapp.js
+++ public/js/myapp.js
@@ -1,20 +1,31 @@
 var app = angular.module('myApp', []);
+app.filter('orderObjectBy', function() {
+  return function(items, field, reverse) {
+    var filtered = [];
+    angular.forEach(items, function(item) {
+      filtered.push(item);
+    });
+    filtered.sort(function (a, b) {
+      return (a[field] > b[field] ? 1 : -1);
+    });
+    if(reverse) filtered.reverse();
+    return filtered;
+  };
+});
 app.controller('dealsCtrl', function($scope, $http) {
-  $scope.dealsData = "Loading...";
   $http.get("https://www.cheapshark.com/api/1.0/deals").then(function (response) {
-  //$http.get("http://deals.dev/deals.json").then(function (response) {
+  //$http.get("deals.json").then(function (response) { //for faster local development
     $scope.dealsData = response.data;
     $scope.groupedDeals = { };
 
     function roundedScore(score) {
-      var rounded = Math.round(score / 10) * 10;
+      var rounded = Math.ceil(score / 10) * 10;
       return rounded;
     };
-
-    $scope.dealsData.forEach(function(currentValue, index, array) {
+    for ( var index in $scope.dealsData ) {
+      var array = $scope.dealsData;
       var rounded = roundedScore(array[index].metacriticScore);
       array[index].roundedScore = rounded.toString();
-      //console.log(array[index]);
 
       var list = $scope.groupedDeals[array[index].roundedScore];
 
@@ -23,9 +34,6 @@ app.controller('dealsCtrl', function($scope, $http) {
       } else {
         $scope.groupedDeals[array[index].roundedScore] = [array[index]];
       }
-    });
-    $scope.groupedDeals.forEach(function(currentValue, index, array) {
-      console.log(index);
-    });
+    };
   });
 });
